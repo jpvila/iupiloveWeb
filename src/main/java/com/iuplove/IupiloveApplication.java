f4fe39103ed42cf4992cce2bb23eea9aeeff1a67
Juan Pablo <jp@gmail.com>
2020-01-21 17:05:26 -0300

    first commit

---
+++ src/main/java/com/iuplove/IupiloveApplication.java
@@ -0,0 +1,13 @@
+package com.iuplove;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IupiloveApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IupiloveApplication.class, args);
+	}
+
+}
